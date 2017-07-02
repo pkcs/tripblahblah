1e5150bad5ac6eff373889aa7799f0b5efd1928d
owlur <8864248@naver.com>
2017-07-02 21:48:47 +0900

    update_AuthActivity

---
+++ app/src/main/java/pkcs/tripblahblah/Activity/AuthActivity.java
@@ -0,0 +1,15 @@
+package pkcs.tripblahblah.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pkcs.tripblahblah.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
