eb549da01297887b9f16bf26fe9e52d597aa69d6
DESKTOP-S9KGBF5\bgh29 <bgh2982@gmail.com>
2017-07-02 14:24:00 +0900

    - Add picasso library

--- app/src/main/java/pkcs/tripblahblah/Activity/MainActivity.java
+++ app/src/main/java/pkcs/tripblahblah/Activity/MainActivity.java
@@ -1,4 +1,4 @@
-package pkcs.tripblahblah;
+package pkcs.tripblahblah.Activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import pkcs.tripblahblah.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
