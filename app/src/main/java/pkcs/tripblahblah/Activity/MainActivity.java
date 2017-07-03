1dd38de6b6e02da46b8e7eabd5e0cbf4daeb3b8d
jinwung <baseball0020@gmail.com>
2017-07-03 19:45:55 +0900

    google auth and login

--- app/src/main/java/pkcs/tripblahblah/Activity/MainActivity.java
+++ app/src/main/java/pkcs/tripblahblah/Activity/MainActivity.java
@@ -1,7 +1,9 @@
 package pkcs.tripblahblah.Activity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -15,15 +17,37 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.Toast;
 
-import pkcs.tripblahblah.Dialog.LoginDialog;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 import pkcs.tripblahblah.Fragment.AttractionFragment;
 import pkcs.tripblahblah.Fragment.GuideFragment;
 import pkcs.tripblahblah.Fragment.TripPlanFragment;
 import pkcs.tripblahblah.R;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.OnConnectionFailedListener {
+
+
+    FirebaseAuth mFirebaseAuth;
+    FirebaseUser mFirebaseUser;
+
+
+    GoogleApiClient mGoogleApiClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,23 +56,42 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build();
+
+        if(mFirebaseUser == null) {
+            showGoogleLogin();
+        }
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        LoginDialog dialog = new LoginDialog(this);
-        dialog.getWindow().clearFlags( WindowManager.LayoutParams.FLAG_DIM_BEHIND);
-        WindowManager.LayoutParams params = dialog.getWindow().getAttributes();
-        dialog.getWindow().setAttributes(params);
-        dialog.show();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         getSupportFragmentManager().beginTransaction().replace( R.id.content_main, TripPlanFragment.newInstance() ).commit();
 
+    }
+
+    public void showGoogleLogin(){
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(intent, 100);
     }
 
     @Override
@@ -95,12 +138,38 @@ public class MainActivity extends AppCompatActivity
             getSupportFragmentManager().beginTransaction().replace( R.id.content_main, AttractionFragment.newInstance() ).commit();
         } else if (id == R.id.nav_guide){
             getSupportFragmentManager().beginTransaction().replace( R.id.content_main, GuideFragment.newInstance() ).commit();
-        } else if( id == R.id.nav_logout) {
-            Log.i("test", "touch");
+        }else if( id == R.id.nav_logout) {
+            mFirebaseAuth.signOut();
+            Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+            showGoogleLogin();
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
 
         return true;
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ( requestCode == 100) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if ( result.isSuccess() ) {
+                String token = result.getSignInAccount().getIdToken();
+                AuthCredential credential = GoogleAuthProvider.getCredential(token, null);
+
+                mFirebaseAuth.signInWithCredential(credential);
+                mFirebaseUser = mFirebaseAuth.getCurrentUser();
+            }
+            else {
+                showGoogleLogin();
+                //Log.d( "Google Login Failed." + result.getStatus());
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
